bd51bf7325b7d8248679d2880493e9bfdeea1046
Vinicius Brisotti <vinicius.brisotti@hotmail.com>
2023-12-26 14:31:16 -0300

    Removing unnecessary blank space

--- src/main/java/com/example/springboot/model/ProductModel.java
+++ src/main/java/com/example/springboot/model/ProductModel.java
@@ -10,7 +10,6 @@ import java.util.UUID;
 @Table(name = "TB_PRODUCTS")
 public class ProductModel implements Serializable {
     private static final long serialVersionUID = 1L;
-
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private UUID idProduct;
