ab3aa4631ca52123019d5df0e220e5d5a4ecca89
Vinicius Brisotti <vinicius.brisotti@hotmail.com>
2023-12-26 21:24:33 -0300

    Method POST created

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -1,8 +1,18 @@
 package com.example.springboot.controllers;
 
+import com.example.springboot.dtos.ProductRecordDto;
+import com.example.springboot.model.ProductModel;
 import com.example.springboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ProductController {
@@ -10,5 +20,10 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
-
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
 }
